1417b4a6143a7cf34228812fe9b2d0d117fcc4d0
Marek Schmidt <fregaham@gmail.com>
2011-07-06 13:40:36 +0200

    add license information

---
+++ README.txt
@@ -0,0 +1,21 @@
+
+ManaClash
+
+A certain trading card game engine in Python.
+
+Authors:
+    Marek Schmidt <fregaham AT gmail DOT com>
+
+http://manaclash.org
+
+ManaClash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ManaClash is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+
--- io.py
+++ io.py
@@ -1,6 +1,63 @@
+# Copyright 2011 Marek Schmidt
+# 
+# This file is part of ManaClash
+#
+# ManaClash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ManaClash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ManaClash.  If not, see <http://www.gnu.org/licenses/>.
+#
+# 
 
 from objects import *
 from abilities import *
+
+greeting = """ManaClash  Copyright (C) 2011  Marek Schmidt
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `warranty'
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `license' for details.
+"""
+
+warranty = """Disclaimer of Warranty.
+
+  THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+
+  Limitation of Liability.
+
+  IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS
+THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY
+GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE
+USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF
+DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD
+PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS),
+EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+  Interpretation of Sections 15 and 16.
+
+  If the disclaimer of warranty and limitation of liability provided
+above cannot be given local legal effect according to their terms,
+reviewing courts shall apply local law that most closely approximates
+an absolute waiver of all civil liability in connection with the
+Program, unless a warranty or assumption of liability accompanies a
+copy of the Program in return for a fee.
+"""
 
 class Output:
     def deleteObject(self, obj):
@@ -16,9 +73,10 @@ class Output:
         print "Creating zone %d %s %s" % (id, str(owner), name)
 
 def input_generator ():
-    print "pre first yield"
+
+    print greeting
+
     _as = yield None
-    print "post first yield: " + `_as`
 
     log = []
 
@@ -59,6 +117,13 @@ def input_generator ():
                     print `log`
                 if _input == "exit":
                     return
+                if _input == "warranty":
+                    print warranty
+                if _input == "license":
+                    lf = open("LICENSE.txt", "r")
+                    for line in lf:
+                        print line.rstrip()
+                    lf.close()
                 selected = int(_input)
                 log.append(selected)
             except ValueError, x:
