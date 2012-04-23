db377a056a95a7fed442a0fb5aa738c27c7abe12
Marek Schmidt <fregaham@gmail.com>
2012-04-23 23:32:00 +0200

    some work on abserver

--- abserver.py
+++ abserver.py
@@ -33,6 +33,140 @@ from autobahn.wamp import exportRpc, \
                           WampClientProtocol
 
 Context = threading.local()
+g_factory = None
+
+def player_to_role(game, player):
+    for i in range(len(game.players)):
+        if game.players[i] == player:
+            return "player" + str(i + 1)
+   
+    raise Exception("No such player in the game.") 
+
+def role_to_player(game, role):
+    return game.players[int(role[len("player"):]) - 1]
+
+def object_to_map(game, o):
+    ret = {}
+    ret["id"] = o.id
+    ret["title"] = o.get_state().title
+    ret["text"] = o.get_state().text
+
+    return ret
+
+def zone_to_list(game, zone):
+    ret = []
+    for o in zone.objects:
+        ret.append (object_to_map(game, o))
+    return ret
+
+def ab_input_generator():
+    seed = random.randint(0,2**64)
+    random.seed(seed)
+
+    _as = yield None
+
+    while True:
+
+        # send the game state
+        state = {}
+        state["player"] = player_to_role(_as.game, _as.player)
+        state["text"] = _as.text
+        state["turn"] = player_to_role(_as.game, _as.game.get_active_player())
+        state["phase"] = _as.game.current_phase
+        state["step"] = _as.game.current_step
+       
+        state["in_play"] = zone_to_list(_as.game, _as.game.get_in_play_zone())
+        state["stack"] = zone_to_list(_as.game, _as.game.get_stack_zone())
+
+        players = []
+        for player in _as.game.players:
+            p = {}
+            p["role"] = player_to_role(_as.game, player)
+            p["name"] = player.name
+            p["life"] = player.life
+            p["manapool"] = player.manapool
+            p["hand"] = zone_to_list(_as.game, _as.game.get_hand(player))
+            p["library"] = zone_to_list(_as.game, _as.game.get_library(player))
+            p["graveyard"] = zone_to_list(_as.game, _as.game.get_graveyard(player))
+
+            players.append(p)
+
+        state["players"] = players
+
+        actions = None
+        query = None
+        if isinstance(_as, ActionSet):
+            actions = []
+            for a in _as.actions:
+                am = {}
+                am["text"] = a.text
+                if a.object is not None:
+                    am["object"] = a.object.id
+                if a.ability is not None:
+                    am["ability"] = a.ability.get_text(a.object)
+                if a.player is not None:
+                    am["player"] = player_to_role(_as.game, a.player)
+                actions.append(am)
+
+        elif isinstance(_as, QueryNumber):
+            query = "Enter number: "
+
+        state["actions"] = actions
+        state["query"] = query
+
+        action = None
+        while action == None:
+            i = 0
+
+            if isinstance(_as, ActionSet):
+                for a in _as.actions:
+                    print ("%d: %s"  % (i, a.text))
+                    i += 1
+            elif isinstance(_as, QueryNumber):
+                print("Enter number: ")
+
+
+            try:
+                # automatically pass if only mana abilities possible in a
+                # priority
+
+                autopass = True
+                if _as.text != "You have priority":
+                    autopass = False
+                if isinstance(_as, ActionSet):
+                    for a in _as.actions:
+                        if a.text != "Pass" and not isinstance(a.ability, BasicManaAbility):
+                            autopass = False
+
+                if autopass:
+                    _input = "0"
+                else:
+                    _input = input()
+
+                if _input == "log":
+                    print("seed: %d" % seed)
+                    print(repr(log))
+                if _input == "exit":
+                    return
+                if _input == "warranty":
+                    print(warranty)
+                if _input == "license":
+                    lf = open("LICENSE.txt", "r")
+                    for line in lf:
+                        print(line.rstrip())
+                    lf.close()
+                selected = int(_input)
+                log.append(selected)
+            except ValueError:
+                selected = -1
+
+            if isinstance(_as, ActionSet):
+                if selected >= 0 and selected < len(_as.actions):
+                    action = _as.actions[selected]
+            elif isinstance(_as, QueryNumber):
+                action = selected
+
+        _as = yield action
 
 class Game:
     def __init__ (self, id):
@@ -50,6 +184,9 @@ class Game:
         player.user.players.append(player)
 
         Context.current_protocol.dispatch("http://manaclash.org/game/" + str(self.id) + "/add", (player.user.login, player.role))
+
+    def start(self):
+        pass
 
 class Player:
     def __init__ (self, user, game, role):
@@ -113,9 +250,11 @@ class MyServerProtocol(WampServerProtocol):
         ## register a single, fixed URI as PubSub topic
         self.registerForPubSub("http://manaclash.org/users")
         self.registerForPubSub("http://manaclash.org/games")
+        self.registerForPubSub("http://manaclash.org/game/", prefixMatch=True)
 
         self.registerMethodForRpc("http://manaclash.org/login", self, MyServerProtocol.onLogin)
-        self.registerMethodForRpc("http://manaclash.org/game/create", self, MyServerProtocol.onGameCreate)
+        self.registerMethodForRpc("http://manaclash.org/games/create", self, MyServerProtocol.onGameCreate)
+        self.registerMethodForRpc("http://manaclash.org/games/join", self, MyServerProtocol.onGameJoin)
 
         self.registerHandlerForSub("http://manaclash.org/users", self, MyServerProtocol.onUsersSub, prefixMatch=False)
         self.registerHandlerForSub("http://manaclash.org/games", self, MyServerProtocol.onGamesSub, prefixMatch=False)
@@ -171,7 +310,7 @@ class MyServerProtocol(WampServerProtocol):
         global last_game_id
 
         client = client_map.get(self.session_id)
-        if client is not None and client.user is not None:
+        if client is not None and client.user is not None and client.player is None:
             last_game_id += 1
 
             game_id = last_game_id
@@ -185,9 +324,27 @@ class MyServerProtocol(WampServerProtocol):
             game_map[game_id] = game
 
             self.dispatchGames()
-            return game_id
+            return "http://manaclash.org/game/" + str(game_id)
 
         return None
+
+    def onGameJoin(self, game_id, role):
+        Context.current_protocol = self
+
+        assert role == "player2"
+
+        client = client_map.get(self.session_id)
+        if client is not None and client.user is not None and client.player is None:
+            game = game_map.get(game_id)
+            if game is not None and len(game.players) < 2:
+                player = Player(client.user, game, "player2")
+                player.setSessionId(self.session_id)
+
+                game.add(player)
+                self.dispatchGames()
+                return "http://manaclash.org/game/" + str(game.id)
+
+        return False
 
     def dispatchUsers(self, exclude=[], eligible=None):
         self.dispatch("http://manaclash.org/users", map(lambda client:client.user.login, filter(lambda client:client.user is not None, client_map.values())), exclude, eligible)
@@ -221,13 +378,15 @@ class MyServerProtocol(WampServerProtocol):
 #        server_ids.append(self.session_id)
 #        self.subscribe("http://manaclash.org/login", self.onLogin)
 
+
+
 if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
-    factory = WampServerFactory("ws://localhost:9000", debugWamp = True)
-    factory.protocol = MyServerProtocol
-    factory.setProtocolOptions(allowHixie76 = True)
-    listenWS(factory)
+    g_factory = WampServerFactory("ws://localhost:9000", debugWamp = True)
+    g_factory.protocol = MyServerProtocol
+    g_factory.setProtocolOptions(allowHixie76 = True)
+    listenWS(g_factory)
 
     #factory = WampClientFactory("ws://localhost:9000")
     #factory.protocol = MyClientProtocol
