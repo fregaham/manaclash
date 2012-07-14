6b984b39cd6534114ea662d42d513b1601c322aa
Marek Schmidt <fregaham@gmail.com>
2012-07-14 12:10:50 +0200

    forgot file

---
+++ conditions.py
@@ -0,0 +1,44 @@
+# Copyright 2012 Marek Schmidt
+# 
+# This file is part of ManaClash
+#
+# ManaClash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ManaClash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ManaClash.  If not, see <http://www.gnu.org/licenses/>.
+#
+# 
+
+from objects import *
+
+class Condition:
+    def evaluate (self, game, context):
+        return False
+    
+    def __str__(self):
+        return "condition"
+
+class IfXHasNOrLessLife(Condition):
+    def __init__ (self, selector, n):
+        self.selector = selector
+        self.n = n
+
+    def evaluate(self, game, context):
+        # assume it is "any player", not "all players" (mostly it's just you or just opponent anyway)
+        for player in self.selector.all(game, context):
+            if player.life <= self.n:
+                return True
+
+        return False
+
+    def __str__ (self):
+        return "IfXHasNOrLessLife(%s, %s)" % (self.selector, self.n)
+
